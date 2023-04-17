3d32bfdec9c8f3cddb0c99cc8348550514979b2e
pavlushaemae <pavel.pidikseev173@gmail.com>
2023-04-17 13:20:18 +0300

    add rxJava impl

--- app/src/main/java/com/itis/example/domain/location/GetLocationUseCase.kt
+++ app/src/main/java/com/itis/example/domain/location/GetLocationUseCase.kt
@@ -1,12 +1,13 @@
 package com.itis.example.domain.location
 
 import com.itis.example.domain.location.model.LocationModel
+import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
 
 class GetLocationUseCase @Inject constructor(
     private val locationRepository: LocationRepository
 ) {
-    suspend operator fun invoke(): LocationModel? {
+    operator fun invoke(): Single<LocationModel> {
         return locationRepository.getLocation()
     }
 }
--- app/src/main/java/com/itis/example/domain/weather/GetWeatherListUseCase.kt
+++ app/src/main/java/com/itis/example/domain/weather/GetWeatherListUseCase.kt
@@ -1,14 +1,15 @@
 package com.itis.example.domain.weather
 
 import com.itis.example.domain.weather.model.WeatherUIModel
+import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
 
 class GetWeatherListUseCase @Inject constructor(
     private val weatherRepository: WeatherRepository
 ) {
-    suspend operator fun invoke(
+    operator fun invoke(
         lat: Double,
         lon: Double,
         cnt: Int
-    ): List<WeatherUIModel> = weatherRepository.getSomeCities(lat, lon, cnt)
+    ): Single<List<WeatherUIModel>> = weatherRepository.getSomeCities(lat, lon, cnt)
 }
